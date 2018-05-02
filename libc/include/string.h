7bd74e42178e32cfdd3e71683a9b81d5e4dfdc9c
ParadoxZero <sidhin.thomas@gmail.com>
2018-05-02 14:10:06 +0530

    Fixed an error in makefile

--- libc/include/string.h
+++ libc/include/string.h
@@ -24,6 +24,10 @@
 extern "C" {
 #endif
 
+int memcmp(const void*, const void*, size_t);
+void* memcpy(void* __restrict, const void* __restrict, size_t);
+void* memmove(void*, const void*, size_t);
+void* memset(void*, int, size_t);
 size_t strlen(const char*); 
 
 #ifdef __cplusplus
